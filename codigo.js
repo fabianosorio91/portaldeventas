2c3276ce44a9cbc4401cb478b6f9773b040e9f8b
Fabian Osorio <fabianosorio1991@gmail.com>
2021-05-25 06:07:00 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,14 @@
+function addEventListenerListResaltar(list) {
+    for (var i = 0, len = list.length; i < len; i++) {
+        list[i].addEventListener('mouseover', function(){list.forEach(function(e){e.className = "caja resaltar"})}, false);
+        list[i].addEventListener('mouseleave', function(){list.forEach(function(e){e.className = "caja"})}, false);
+    }
+}
+
+function resolveList(list){
+    for (var i = 0, len = list.length; i < len; i++) {
+        list[i].className = "caja resaltar"
+        list[i].removeEventListener('mouseover',function(){console.log("Removed")})
+        list[i].removeEventListener('mouseleave',function(){console.log("Removed")})
+    }
+}
